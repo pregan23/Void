34a70e808f59002d6f76092963d0fb723c9aab29
pregan23 <pregan2bing@gmail.com>
2022-03-28 09:52:56 -0400

    seed to insert two users

---
+++ seed/userSeed.js
@@ -0,0 +1,19 @@
+const db = require('../db')
+const User = require('../models/User')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+    const users = [
+        { userName: 'User1', password: 'User1', conversations: [] },
+        { userName: 'User2', password: 'User2', conversations: [] }
+    ]
+
+    await User.insertMany(users)
+    console.log('inserted User1 and User 2')
+}
+const run = async () => {
+    await main()
+    db.close()
+}
+run()
