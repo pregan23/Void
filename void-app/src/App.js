78793403bb367e0fc80699110c02b6f7bce31e36
pregan23 <pregan2bing@gmail.com>
2022-03-29 12:01:33 -0400

    beginings of login method

--- void-app/src/App.js
+++ void-app/src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [userName, setUserName] = useState('')
+  const [userPass, setUserPass] = useState('')
+  const [conversations, setConversations] = useState([])
+  const [userText, setUserText] = useState('')//what the user types for new message
+  const [edited, setEdited] = useState(false)
+
+  // useEffect(() => {
+
+  // },[])
+
+  const login = async (event) => {
+    event.preventDefault()
+    await axios
+    .post('http://localhost:3001/login', {
+      userName: userName,
+      password: userPass
+    })
+    
+  }
+
+
   return (
     <div className="App">
-      
+      {/* <CreateLoginForm /> */}
     </div>
   );
 }
