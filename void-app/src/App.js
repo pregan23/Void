b28db826628e365144f6b81b5f866c3168bb36fe
pregan23 <pregan2bing@gmail.com>
2022-03-29 13:56:06 -0400

    login form

--- void-app/src/App.js
+++ void-app/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
 
   const [userName, setUserName] = useState('')
-  const [userPass, setUserPass] = useState('')
+  const [password, setPassword] = useState('')
   const [conversations, setConversations] = useState([])
   const [userText, setUserText] = useState('')//what the user types for new message
   const [edited, setEdited] = useState(false)
@@ -19,15 +19,29 @@ function App() {
     await axios
     .post('http://localhost:3001/login', {
       userName: userName,
-      password: userPass
+      password: password
     })
     
+  }
+
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
   }
 
 
   return (
     <div className="App">
-      {/* <CreateLoginForm /> */}
+      <CreateLoginForm
+      userName={userName}
+      password={userPass}
+      handleUserNameChange={handleUserNameChange}
+      handlePasswordChange={handlePasswordChange}
+      login={login}
+      />
     </div>
   );
 }
---
+++ void-app/src/components/CreateLoginForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const CreateLoginForm = (props) => {
+    return (
+        <div className = "form-wrapper">
+            <h1>Void</h1>
+            <br/>
+            <form onSubmit={props.login}>
+                <div>
+                    <label>Username:</label>
+                    <input 
+                    type="text"
+                    value={props.userName}
+                    placeholder="Enter Username..."
+                    onChange={props.handleUserNameChange}                    
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input 
+                    type="text"
+                    value={props.userPass}
+                    placeholder="Enter Password..."
+                    onChange={props.handlePasswordChange}                    
+                    />
+                </div>
+                <div className="button-wrapper">
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
