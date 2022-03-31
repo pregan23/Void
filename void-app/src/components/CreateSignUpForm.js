3eb6132438278b2c5170e34610a4b3227a74f2bc
pregan23 <pregan2bing@gmail.com>
2022-03-31 16:27:33 -0400

    sign up form renders with inputs state handlechanges

--- void-app/src/components/CreateSignUpForm.js
+++ void-app/src/components/CreateSignUpForm.js
@@ -4,10 +4,49 @@ import { useParams } from 'react-router-dom'
 
 const CreateSignUpForm = (props) => {
 
+    const [newUserName, setNewUserName] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+
+    const handleNewUserNameChange = (event) => {
+        setNewUserName(event.target.value)
+    }
+
+    const handleNewPasswordChange = (event) => {
+        setNewPassword(event.target.value)
+    }
+
 
 
     return (
-        <div>Sign Up</div>
+        <div className = "form-wrapper">
+        <br/>
+        <h3>Create an account below. Type carefully!</h3>
+        <form onSubmit={props.signup}>
+            <div>
+                <label>Username:</label>
+                <input 
+                type="text"
+                value={newUserName}
+                placeholder="Choose Username..."
+                onChange={handleNewUserNameChange}                    
+                />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input 
+                type="text"
+                value={newPassword}
+                placeholder="Choose Password..."
+                onChange={handleNewPasswordChange}                    
+                />
+            </div>
+            <div className="button-wrapper">
+                
+                <button type="submit">Submit</button>
+                
+            </div>
+        </form>
+    </div>
     )
 
 
